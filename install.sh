72d805530f9b1f7002ac34ada921578eb0fe9329
Andre S <htkandre@gmail.com>
2020-06-12 12:04:39 -0500

    Updated Installation Script

--- install.sh
+++ install.sh
@@ -1,1 +1,20 @@
-#!/bin/sh
+!/bin/sh
+
+# Setups config
+echo "Installing config"
+if [ ! -d "$HOME/.config" ]
+then
+  mkdir "$HOME/.config"
+fi
+cp ./src/backupsys.config $HOME/.config
+
+# Setups dwmblock and backup script
+echo "Installing dwmblock and backup scripts"
+if [ !-d $HOME/.local/bin ]
+then
+  mkdir -p "$HOME/.local/bin/statusbar"
+fi
+cp ./src/backupsys.shell $HOME/.local/bin
+cp ./src/backupsys.dwmblock $HOME/.local/bin/statusbar
+
+echo "Finish installing application";
---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+echo "Starting uninstall"
+
+rm $HOME/.config/backupsys.config
+
+rm $HOME/.local/bin/backupsys.shell
+rm $HOME/.local/bin/statusbar/backupsys.dwmblock
+
+echo "Finished uninstalling"
